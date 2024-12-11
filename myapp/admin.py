4c234a7336987204feb98523b91f05187ee44c98
1130miurakai <b1023203@fun.ac.jp>
2024-12-11 16:02:01 +0900

    02hakodaterestrauantsのテーブル作成挿入。

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Shop
-
+from .models import Shop, HakodateRestaurant  # HakodateRestaurantをインポート
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'latitude', 'longitude', 'shop_id')
+
+@admin.register(HakodateRestaurant)
+class HakodateRestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'latitude', 'longitude', 'restaurant_id')
+    
--- myapp/models.py
+++ myapp/models.py
@@ -9,4 +9,13 @@ class Shop(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class HakodateRestaurant(models.Model):
+    name = models.CharField(max_length=255)       # 店舗名
+    address = models.TextField()                  # 住所
+    latitude = models.FloatField()                # 緯度
+    longitude = models.FloatField()               # 経度
+    restaurant_id = models.CharField(max_length=255)  # レストランID
+
+    def __str__(self):
+        return self.name
