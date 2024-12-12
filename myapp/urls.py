ec517208b3d89fa840abaadab761fd59f24b390d
Lizqxel <b1023027@fun.ac.jp>
2024-12-12 23:21:47 +0900

    ビュー関数の名前を変更し、ハコダテレストランの情報取得を別の関数に移動。URLパターンを更新。

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'myapp'
 urlpatterns = [
-    path('hakodate_restaurant/', views.hakodate_restaurant_location, name='hakodate_restaurant_location'),
+    path('hakodate_restaurant/', views.Start_Screen, name='Start_Screen'),
     path('screen/', views.screen, name='screen'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import HakodateRestaurant
 
-def hakodate_restaurant_location(request):
-    restraunt = get_object_or_404(HakodateRestaurant, name='たろ吉')
-    context = {
-        'name': restraunt.name,
-        'latitude': restraunt.latitude,
-        'longitude': restraunt.longitude,
-    }
-    return render(request, 'HOME/StartScreen.html', context)
+def Start_Screen(request):
+    return render(request, 'HOME/StartScreen.html')
 
 def screen(request):
-    return render(request, 'HOME/Screen.html')
+    restaurant = get_object_or_404(HakodateRestaurant, name='たろ吉')
+    context = {
+        'name': restaurant.name,
+        'latitude': restaurant.latitude,
+        'longitude': restaurant.longitude,
+    }
+    return render(request, 'HOME/Screen.html', context)
